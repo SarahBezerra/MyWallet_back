c664022890ea15bf3b7ef20cc558981294207532
Sarah <sarahbezerra2013@gmail.com>
2022-02-02 12:56:43 -0300

    Valida dados de cadastro

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import joi from 'joi'
 import { MongoClient, ObjectId } from 'mongodb';
 
 const app = express();
@@ -10,6 +12,20 @@ app.post('/login', (req, res) => {
 })
 
 app.post('/cadastro', (req, res) => {
+    const registrationSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        confirm_password: joi.ref('password')
+    })
+
+    const registrationData = req.body
+    const validation = registrationSchema.validate(registrationData)
+    if(validation.error){
+        return res.sendStatus(422)
+    }
+
+    res.sendStatus(201)
 })
 
 app.listen(5000);
