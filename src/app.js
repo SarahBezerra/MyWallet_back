34bc28396862f6eb8f7d3a8909db6df0cc75248b
Sarah <sarahbezerra2013@gmail.com>
2022-02-04 12:43:50 -0300

    Salva no banco dados de registro de movimentaçao

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt';
 import joi from 'joi'
 import { v4 as uuid } from 'uuid'
 import { MongoClient, ObjectId } from 'mongodb';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
@@ -71,7 +72,6 @@ app.get('/extrato', async (req, res) => {
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
 
-
     if(!token) return res.sendStatus(401);
 
     const session = await db.collection("sessions").findOne({ token });
@@ -104,6 +104,37 @@ app.get('/extrato', async (req, res) => {
     } else {
         res.sendStatus(401);
     }
+})
+
+app.post('/registrar/:tipo_de_registro', async (req, res) => {
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    if(!token) return res.sendStatus(401);
+
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+        return res.sendStatus(401);
+    }
+
+    const { userId } = session;
+
+    const valueRecordSchema = joi.object({
+        value: joi.string().pattern(/^[0-9]+.[0-9]{2}$/).required(),
+        description: joi.string().pattern(/^[A-Za-z0-9]{2,20}$/).required(),
+        type: joi.string().valid('saida', 'entrada').required()
+    })
+
+    const validation = valueRecordSchema.validate(req.body, { abortEarly: false })
+
+    if(validation.error){
+        return res.status(422).send(validation.error.details.map(error => error.message))
+    }
+
+    await db.collection("registries").insertOne({ ...req.body, userId, date: dayjs().format('DD/MM') })
+    res.sendStatus(201)
+
 })
 
 app.listen(5000);
